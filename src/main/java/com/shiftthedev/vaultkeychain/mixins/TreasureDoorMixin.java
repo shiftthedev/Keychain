42a83d80e7e9656b94577c630f7264a7710dd1b9
ShiftTheDev <jorge_pires_12@hotmail.com>
2024-10-12 18:21:50 +0100

    fix textures

--- src/main/java/com/shiftthedev/vaultkeychain/mixins/TreasureDoorMixin.java
+++ src/main/java/com/shiftthedev/vaultkeychain/mixins/TreasureDoorMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = TreasureDoorBlock.class, remap = false)
+@Mixin(value = TreasureDoorBlock.class)
 public abstract class TreasureDoorMixin extends DoorBlock
 {
     public TreasureDoorMixin(Properties p_52737_)
